89588682c0a5298e7fa3ec1543fb6905fde70717
dennis <dennis@popwallet.com>
2021-06-05 23:58:56 -0500

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from "./home/home.component";
+import { ToolsComponent } from "./tools/tools.component";
+import { EnvironmentsComponent } from "./environments/environments.component";
+import { DocumentationComponent } from "./documentation/documentation.component";
+import { ExternalComponent } from "./external/external.component";
+import { OtherComponent } from "./other/other.component";
+import { ContactComponent } from "./contact/contact.component";
+
+const routes: Routes = [
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'tools', component: ToolsComponent },
+  { path: 'envs', component: EnvironmentsComponent },
+  { path: 'docs', component: DocumentationComponent },
+  { path: 'external', component: ExternalComponent },
+  { path: 'other', component: OtherComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,49 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from  '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatCardModule } from "@angular/material/card";
+import { MatExpansionModule } from "@angular/material/expansion";
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EnvironmentsComponent } from './environments/environments.component';
+import { ToolsComponent } from './tools/tools.component';
+import { ContactComponent } from './contact/contact.component';
+import { OtherComponent } from './other/other.component';
+import { ExternalComponent } from './external/external.component';
+import { DocumentationComponent } from './documentation/documentation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EnvironmentsComponent,
+    ToolsComponent,
+    ContactComponent,
+    OtherComponent,
+    ExternalComponent,
+    DocumentationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatCardModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
